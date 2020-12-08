61d9734ab6c472512035eea711c5c7a3efabfc08
Christian Castillo <58743096+christiancastilloporras@users.noreply.github.com>
2020-12-08 15:43:22 -0600

    Update mig.bash

--- mig.bash
+++ mig.bash
@@ -3,7 +3,7 @@
 gateway=$(ip route get 10.0.0.0/8 | grep -Po '(?<=via )(\d{1,3}.){4}')
 alias=$1
 
-clish -c 'add interface eth1 alias $alias' -s
+clish -c 'add interface eth1 alias "$alias/32"' -s
 clish -c 'set static-route 130.211.0.0/22 nexthop gateway address $gateway on' -s
 clish -c 'set static-route 35.191.0.0/16 nexthop gateway address $gateway on' -s
 
