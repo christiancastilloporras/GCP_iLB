e6ac17d48f277258aa66daa984fab2dbc6e3d4de
Christian Castillo <58743096+christiancastilloporras@users.noreply.github.com>
2020-12-09 14:24:40 -0600

    Update mig.bash

--- mig.bash
+++ mig.bash
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-gateway=<change for next hop>
-alias=<change for ilb ip>
-
-clish -c 'add interface eth1 alias $alias' -s
-clish -c 'set static-route 130.211.0.0/22 nexthop gateway address $gateway on' -s
-clish -c 'set static-route 35.191.0.0/16 nexthop gateway address $gateway on' -s
+clish -c 'add interface eth1 alias <change for ilb ip with mask>' -s
+clish -c 'set static-route 130.211.0.0/22 nexthop gateway address <change for the gateway IP on eth1 subnet> on' -s
+clish -c 'set static-route 35.191.0.0/16 nexthop gateway address <change for the gateway IP on eth1 subnet> on' -s
 
 exit 0
